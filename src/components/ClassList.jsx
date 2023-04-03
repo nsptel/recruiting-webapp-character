d717e888081f819385e9acee04f6c93f73955da1
Nilkumar Patel <nilkumarpatel48@gmail.com>
2023-04-03 14:10:17 -0400

    Show character requirements on click

--- src/components/ClassList.jsx
+++ src/components/ClassList.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { CLASS_LIST } from "../consts";
 
 export default function ClassList({ attrList }) {
   const classList = Object.keys(CLASS_LIST).map((cls, ind) => ({
-    id: ind + 1,
+    id: ind,
     name: cls,
     attrs: CLASS_LIST[cls],
     meetsMin:
       attrList.filter((attr) => CLASS_LIST[cls][attr.name] <= attr.value)
         .length === attrList.length,
   }));
+  const [requirements, setRequirements] = useState();
+
+  const displayRequirements = (key) => {
+    const attrs = classList[key].attrs;
+    setRequirements(
+      Object.entries(attrs).map(([key, val], ind) => ({ id: ind, key, val }))
+    );
+  };
 
   return (
     <div style={{ marginBottom: "10px" }}>
@@ -19,13 +28,23 @@ export default function ClassList({ attrList }) {
               marginRight: "5px",
               marginLeft: "5px",
               color: item.meetsMin ? "green" : "black",
+              cursor: "pointer",
             }}
             key={item.id}
+            onClick={() => displayRequirements(item.id)}
           >
             {item.name}
           </span>
         );
       })}
+      {requirements &&
+        requirements.map((req) => {
+          return (
+            <span style={{ display: "block" }} key={req.id}>
+              {req.key}: {req.val}
+            </span>
+          );
+        })}
     </div>
   );
 }
